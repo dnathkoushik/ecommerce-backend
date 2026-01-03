bacf39266cebd56434c7a2d1a0e24fe1532443c0
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-03 19:23:03 +0530

    Created the logic of inserting admin user at the starting of the application,  created database and connection with the database and started the server initially

---
+++ server.js
@@ -0,0 +1,61 @@
+/**
+ * This will be the strating file of the project
+ */
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const server_config = require("./configs/server.config");
+const db_config = require("./configs/db.config");
+const user_model = require("./models/user.model");
+const bcrypt = require("bcryptjs");
+
+/**
+ * Create an admin user at the starting of the application
+ * if not already present
+ */
+
+
+//Connection with mongodb
+mongoose.connect(db_config.DB_URL)
+const db = mongoose.connection;
+
+db.on("error", () => {
+    console.log("Error while connecting to the mongoDB");
+})
+
+db.once("open", () => {
+    console.log("Connected to mongoDB");
+    init();
+})
+
+async function init(){
+    let user = await user_model.findOne({userId : "admin"});
+    if(user){ //if admin already exist
+        console.log("Admin is already present");
+        return;
+    }
+
+    try{
+
+        user = await user_model.create({
+            name : "koushik",
+            userId : "admin",
+            email : "koushikcomhere@gmail.com",
+            userType : "ADMIN",
+            password : bcrypt.hashSync("Welcome1", 8)
+        });
+        console.log("Admin created ", user)
+
+    } catch(err){
+        console.log("Error while creating admin", err);
+    }
+}
+
+
+/**
+ * Start the server
+ */
+app.listen(server_config.PORT, () => {
+    console.log("Server started at PORT num : ", server_config.PORT);
+});
