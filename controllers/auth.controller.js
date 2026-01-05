fff2baebc8ebb17300e2f28700d9e823b2b86345
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-05 19:58:20 +0530

    Login API created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,8 @@
 
 const bcrypt = require("bcryptjs");
 const user_model = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const secret = require("../configs/auth.config").secret;
 
 exports.signup = async (req, res) => {
     /**
@@ -42,4 +44,35 @@ exports.signup = async (req, res) => {
     }
 
     //3. Return the response back to the user
+}
+
+exports.signin = async (req, res) => {
+    //Check if the userId is present in the system
+    const user = await user_model.findOne({userId : req.body.userId});
+    if(!user){
+        return res.status(400).send({
+            message : "Failed! UserId does not exist, Create an account first then try to signin",
+        });
+    }
+
+    //Passwords match or not
+    const passwordIsValid = bcrypt.compareSync(req.body.password, user.password); //true or false
+    if(!passwordIsValid){
+        return res.status(401).send({
+            message : "Invalid Password ! Try again",
+        });
+    }
+
+    //If all good, generate a JWT access token with a given TTL and return to the user
+    const token = jwt.sign({id : user.userId}, secret, {
+        expiresIn : 120, //2 minutes
+    });
+
+    res.status(200).send({
+        name : user.name, 
+        userId : user.userId,
+        email : user.email, 
+        userType : user.userType,
+        accessToken : token
+    });
 }
--- routers/auth.routes.js
+++ routers/auth.routes.js
@@ -8,4 +8,5 @@ const authMW = require("../middlewares/auth.mw");
 
 module.exports = (app) => {
     app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup);
+    app.post("/ecomm/api/v1/auth/signin", authController.signin);
 }
