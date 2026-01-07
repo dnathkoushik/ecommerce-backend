303fa0252a486d10c17815632240e218919a2f7d
Koushik Debnath <koushikcomhere@gmail.com>
2026-01-07 22:36:51 +0530

    Get product by ID API created

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 const product_model = require('../models/product.model');
 const category_model = require('../models/category.model');
+const mongoose = require('mongoose');
 
 exports.createProduct = async (req, res) => {
     try {
@@ -35,6 +36,36 @@ exports.getAllProducts = async (req, res) => {
         console.log("Error while fetching products", err);
         return res.status(500).send({
             message: "Some internal error while fetching products"
+        });
+    }
+};
+
+exports.getProductById = async (req, res) => {
+    const product_id = req.params.productId;
+
+    if (!mongoose.Types.ObjectId.isValid(product_id)) {
+        return res.status(400).send({
+            message: "Invalid product ID"
+        });
+    }
+
+    try {
+        const product = await product_model
+            .findById(product_id)
+            .populate("category", "name description")
+            .populate("createdBy", "name email userType");
+
+        if (!product) {
+            return res.status(404).send({
+                message: "Product not found"
+            });
+        }
+
+        return res.status(200).send(product);
+
+    } catch (err) {
+        return res.status(500).send({
+            message: "Some internal error while fetching product"
         });
     }
 };
